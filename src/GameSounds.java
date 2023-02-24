8a294996f9a35136f571ebc0e328ec9dcd762ceb
s4elvale <s4elvale@uni-trier.de>
2023-02-24 17:52:07 +0100

    Removes unused url Variable

--- src/GameSounds.java
+++ src/GameSounds.java
@@ -16,25 +16,21 @@ public class GameSounds{
 
 /* Initialize audio files */ 
     public GameSounds(){
-        stopped=true; 
-        URL url;
+        stopped=true;
         AudioInputStream audioIn;
         
         try{
             // src.Pacman eating sound
-            url = this.getClass().getClassLoader().getResource("sounds/nomnom.wav");
             audioIn = AudioSystem.getAudioInputStream(new File ("sounds/nomnom.wav"));
             nomNom = AudioSystem.getClip();
             nomNom.open(audioIn);
             
             // newGame
-            url = this.getClass().getClassLoader().getResource("sounds/newGame.wav");
             audioIn = AudioSystem.getAudioInputStream(new File ("sounds/newGame.wav"));
             newGame = AudioSystem.getClip();
             newGame.open(audioIn);
             
             // death        
-            url = this.getClass().getClassLoader().getResource("sounds/death.wav");
             audioIn = AudioSystem.getAudioInputStream(new File ("sounds/death.wav"));
             death = AudioSystem.getClip();
             death.open(audioIn);
